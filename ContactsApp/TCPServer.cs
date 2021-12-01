be63265468d206a377322b962ad15d75964cddf0
M2KV <wgmin.it@outlook.com>
2021-12-01 15:13:18 +0700

    Server Front End

---
+++ ContactsApp/TCPServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    internal class TCPServer
+    {
+        public TCPServer(string ip, string  port, long bytes)
+        {
+
+        }
+    }
+}
