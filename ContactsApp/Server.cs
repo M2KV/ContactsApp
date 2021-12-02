764f4d19913caa1b96e59f80556c937246c382b8
M2KV <wgmin.it@outlook.com>
2021-12-02 11:52:52 +0700

    new update

--- ContactsApp/Server.cs
+++ ContactsApp/Server.cs
@@ -69,13 +69,63 @@ namespace ContactsApp
             IPBox.ForeColor     = (AutomaticBox.Checked) ? SystemColors.WindowText : SystemColors.WindowFrame;
             PortBox.ForeColor   = (AutomaticBox.Checked) ? SystemColors.WindowText : SystemColors.WindowFrame;
 
-            IPBox.Text   = (!AutomaticBox.Checked)   ? "Input IP"    : "172.0.0.1";
+            IPBox.Text   = (!AutomaticBox.Checked)   ? "Input IP"    : "127.0.0.1";
             PortBox.Text = (!AutomaticBox.Checked)   ? "Input Port"  : "8080";
         }
 
         private void RunServer_Click(object sender, EventArgs e)
         {
+            if (_Server == null)
+            {
+                _Server = new TCPServer(IPBox.Text, PortBox.Text, 10485760);
+                _Server.ConsoleLogger = ConsoleLogger;
+                _Server.ConsoleStatus = ConsoleStatus;
 
+                _Server.Run();
+
+                if (_Server.connected)
+                {
+                    StopServer.Enabled = true;
+                    RunServer.Enabled = false;
+
+                    _ = MessageBox.Show("Server is running !", "Server running", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else 
+                    _ = MessageBox.Show("Server cannot run !", "Server error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static TCPServer _Server = null;
+
+        private void StopServer_Click(object sender, EventArgs e)
+        {
+            if (_Server.connected)
+            {
+                _Server.Stop();
+                
+                if (!_Server.connected)
+                {
+                    _Server = null;
+
+                    StopServer.Enabled = false;
+                    RunServer.Enabled = true;
+                    _ = MessageBox.Show("Server stopped !", "Server disconnect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    _ = MessageBox.Show("Server cannot stop !", "Server error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Server_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_Server != null && _Server.connected)
+            {
+                _Server.Stop();
+                _Server = null;
+
+                StopServer.Enabled = false;
+                RunServer.Enabled = true;
+            }
         }
     }
 }
--- ContactsApp/TCPServer.cs
+++ ContactsApp/TCPServer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,117 @@ namespace ContactsApp
 {
     internal class TCPServer
     {
-        public TCPServer(string ip, string  port, long bytes)
+        public TCPServer(string ip, string  port, int bytes)
         {
+            _IP         = IPAddress.Parse(ip);
+            _Port       = int.Parse(port);
+            _Bytes      = bytes;
+            _Buffers    = new byte[bytes];
+            connected   = false;
+            _Clients    = new List<Socket>();
+        }
 
+        private void InvokeConsole(RichTextBox console, string text, bool append = false)
+        {
+            if (!console.IsDisposed)
+            {
+                _ = console.Invoke(new MethodInvoker(delegate {
+                    if (!append) console.Text = text;
+                    else console.Text += text;
+                }));
+            }
+        }
+
+        public void Run()
+        {
+            try
+            {
+                _Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _Socket.Bind(new IPEndPoint(_IP, _Port));
+                _Socket.Listen(0);
+                _Socket.BeginAccept(AcceptCallback, null);
+                
+                connected = true;
+                InvokeConsole(_ConsoleLogger, "\n   Server running successfully !\n", true);
+            }
+            catch (SocketException ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Socket create error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = _Socket.EndAccept(ar);
+                _Clients.Add(socket);
+
+                socket.BeginReceive(_Buffers, 0, _Bytes, SocketFlags.None, ReceiveCallback, socket);
+                _Socket.BeginAccept(AcceptCallback, null);
+            }
+            catch (ObjectDisposedException) { }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = ar.AsyncState as Socket;
+                int bytes = socket.EndReceive(ar);
+
+                if (bytes > 0)
+                {
+                    Requests req = new Requests(_Buffers, bytes);
+                    switch (req.action)
+                    {
+                        case "1":
+                            break;
+                    }    
+                }    
+            }
+            catch (SocketException) { }
+            catch (ObjectDisposedException) { }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                Responses res = new Responses("Disconnected", "Server was disconnected");
+                foreach (Socket socket in _Clients)
+                {
+                    socket.Send(res.toBytes());
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                _Socket.Close();
+
+                connected = false;
+                InvokeConsole(_ConsoleLogger, "   Server stopped !\n", true);
+                GC.Collect(0);
+            }
+            catch { }
+        }
+
+        private static Socket _Socket;
+        private static List<Socket> _Clients;
+        private static IPAddress _IP;
+        private static int _Port;
+        private static int _Bytes;
+        private static byte[] _Buffers;
+        private RichTextBox _ConsoleLogger;
+        private RichTextBox _ConsoleStatus;
+
+        public bool connected;
+        public RichTextBox ConsoleLogger
+        {
+            set { _ConsoleLogger = value; }
+        }
+
+        public RichTextBox ConsoleStatus
+        {
+            set { _ConsoleStatus = value; }
         }
     }
 }
